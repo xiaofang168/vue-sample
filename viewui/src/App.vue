fb557b78ddb43bbdad60958802be7387e9e1dd3b
FangJie <jiefffang@didiglobal.com>
2022-03-28 19:19:50 +0800

    使用菜单

--- viewui/src/App.vue
+++ viewui/src/App.vue
@@ -22,21 +22,20 @@
 				<Content :style="{padding: '24px 0', minHeight: '280px'}">
 					<Layout>
 						<Sider hide-trigger>
-							<Menu active-name="1-1" width="auto" :open-names="open_names">
+							<Menu active-name="1-1" width="auto" :open-names="open_names" :accordion="true" @on-select="handleMenu">
 								<Submenu name="1">
 									<template slot="title">
 										<Icon type="ios-navigate"></Icon>
 										系统管理
 									</template>
-									<MenuItem name="1-1">配置列表</MenuItem>
+									<MenuItem name="settings">配置列表</MenuItem>
 									<MenuItem name="1-2">数据查询</MenuItem>
 								</Submenu>
 							</Menu>
 						</Sider>
 						<Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
 							<router-link to="/">Home</router-link> |
-							<router-link to="/about">About</router-link>|
-							<router-link to="/settings">Setting</router-link>
+							<router-link to="/about">About</router-link>
 							<Spin v-if="show_loading" size="large" fix />
 							<div v-else>
 								<!-- 路由出口 -->
@@ -55,14 +54,21 @@
 		name: 'App',
 		data() {
 			return {
-				open_names:['1'],
+				open_names: ['1'],
 				show_loading: true
 			};
 		},
 		async created() {
 			setTimeout(() => {
 				this.show_loading = false
-			}, 2000);
+			}, 1000);
+		},
+		methods: {
+			handleMenu(val) {
+				this.$router.push({
+					path: val
+				});
+			}
 		}
 	}
 </script>
